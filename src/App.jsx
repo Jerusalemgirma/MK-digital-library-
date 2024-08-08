5ecfed4fba10369899376ae3fb83ef626c5a016c
NMBY21 <betelihemmengist21@gmail.com>
2024-08-08 13:31:46 +0300

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { Button, Flex, Layout } from 'antd';
-import {useState} from 'react';
-import {MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons'
+import { Button, Layout } from 'antd';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import Sidebar from './Components/Sidebar/Sidebar';
-import CustomHeader from './Components/Header/Header'
-import MainContent from './Components/Content/MainContent'
-import SideContent from './Components/Content/SideContent'
+import CustomHeader from './Components/Header/Header';
+import MainContent from './Components/Content/MainContent';
+import SideContent from './Components/Content/SideContent';
+import Home from './Components/Pages/Home/home';
+import Ebook from './Components/Pages/Ebook/ebook';
+import Audio from './Components/Pages/Audio/audio';
+import Video from './Components/Pages/Video/video';
+import Magazines from './Components/Pages/Magazines/magazines';
+import MyActivity from './Components/Pages/MyActivity/myactivity';
+import History from './Components/Pages/History/history';
+import Categories from './Components/Pages/Categories/categories';
 
-import './App.css'
+import './App.css';
 
-const {Sider, Header, Content}=Layout;
+const { Sider, Header, Content } = Layout;
 
- const App = () => {
-   const [collapsed, setCollapsed] = useState(false)
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-  <Layout>
-    <Sider theme='light' trigger={null} collapsed={collapsed} className='sider'>
-      <Sidebar/>
-      <Button type='text' icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>} onClick={()=> setCollapsed(!collapsed)} className='triger-btn'/>
-    </Sider>
-    <Layout>
-      <Header className='header'>
-        <CustomHeader/>
-      </Header>
-      <Content className='content'>
-        <Flex gap="large">
-          <MainContent/>
-          <SideContent/>
-        </Flex>
-      </Content>
-    </Layout>
-  </Layout>
+    <Router>
+      <Layout>
+        <Sider theme='light' trigger={null} collapsed={collapsed} className='sider'>
+          <Sidebar />
+          <Button
+            type='text'
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            className='trigger-btn'
+          />
+        </Sider>
+        <Layout>
+          <Header className='header'>
+            <CustomHeader />
+          </Header>
+          <Content className='content'>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/ebook" element={<Ebook />} />
+              <Route path="/audio" element={<Audio />} />
+              <Route path="/video" element={<Video />} />
+              <Route path="/magazines" element={<Magazines />} />
+              <Route path="/myactivity" element={<MyActivity />} />
+              <Route path="/history" element={<History />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route
+                path="/"
+                element={
+                  <div style={{ display: 'flex', gap: 'large' }}>
+                    <MainContent />
+                    <SideContent />
+                  </div>
+                }
+              />
+            </Routes>
+          </Content>
+        </Layout>
+      </Layout>
+    </Router>
   );
-}
+};
+
 export default App;
