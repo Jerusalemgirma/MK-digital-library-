f85b69ff0eb416c6cc01515aba135b93ed93e576
NMBY21 <betelihemmengist21@gmail.com>
2024-08-12 13:45:10 +0300

    my activity_routes

--- src/Components/Header/ProfilePopup.jsx
+++ src/Components/Header/ProfilePopup.jsx
@@ -23,7 +23,7 @@ const ProfilePopup = ({ onClose }) => {
   }, [onClose]);
 
   const handleManageAccountClick = () => {
-    navigate('/manageaccount'); 
+    navigate('/myactivity'); 
     onClose(); 
   };
 
