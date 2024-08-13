3ab60e0abf3b18b0eb23b40cfeb83952ba67d4cb
NMBY21 <betelihemmengist21@gmail.com>
2024-08-13 10:29:28 +0300

    edit myactivity

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Audio from './Components/Pages/Audio/audio';
 import Video from './Components/Pages/Video/video';
 import Magazines from './Components/Pages/Magazines/magazines';
 import ReadMagazine from './Components/Pages/Magazines/readMagazine.jsx';
-import MyActivity from './Components/Pages/MyActivity/myactivity';
+import ManageAccount from './Components/Pages/MyActivity/myactivity';
 import History from './Components/Pages/History/history';
 import Categories from './Components/Pages/Categories/categories';
 import EditAbout from './Components/Pages/MyActivity/Navs/editabout'; 
@@ -47,7 +47,7 @@ const App = () => {
               <Route path="/video" element={<Video />} />
               <Route path="/magazines" element={<Magazines />} />
               <Route path="/read/:id" element={<ReadMagazine />} />
-              <Route path="/myactivity" element={<MyActivity />} />
+              <Route path="/myactivity" element={<ManageAccount />} />
               <Route path="/history" element={<History />} />
               <Route path="/categories" element={<Categories />} />
               <Route path="/editabout" element={<EditAbout />} />
